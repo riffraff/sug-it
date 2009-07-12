e56b4ecc331223656f43b9bd8287ecf8f9b9b161
gabriele renzi <rff.rff@gmail.com>
2009-07-12 15:26:45 +0200

    clean

--- prisma/prisma.scala
+++ prisma/prisma.scala
@@ -10,52 +10,52 @@ import scala.util.parsing.json.JSON.parseFull
     }
     val DEFAULT = mkPath(List(System.getProperty("user.home"), ".prisma"))
 
-            def follow(x: String) = {
-            if (file(DEFAULT, x) createNewFile)
-                println("following "+x)
-            else
-                println("already following "+ x)
+    def follow(x: String) = {
+        if (file(DEFAULT, x) createNewFile)
+            println("following "+x)
+        else
+            println("already following "+ x)
+    }
+
+    def show (x: String)= println("Data is "+ search(x))
+
+    def list() = println("Following\n"+ formatListing)
+
+    def read(key:String):String = io.Source.fromURL("http://search.twitter.com/search.json?q="+key+ "&since="+lastAccess(key) ) mkString
+    def lastAccess(key: String): String =  "2009-07-09" //new Date(file(DEFAULT, key).lastModified)
+    def search(key: String): String = getTexts(parseFull(read(key))) mkString "\n"
+
+    def getTexts( p :Option[Any]): Seq[String] =  {
+        val results = p match {
+            case None => return Nil
+            case Some( x :Map[String,Any]) => x 
         }
-
-        def show (x: String)= println("Data is "+ search(x))
-
-        def list() = println("Following\n"+ formatListing)
-
-        def read(key:String):String = io.Source.fromURL("http://search.twitter.com/search.json?q="+key+ "&since="+lastAccess(key) ) mkString
-        def lastAccess(key: String): String =  "2009-07-09" //new Date(file(DEFAULT, key).lastModified)
-        def search(key: String): String = getTexts(parseFull(read(key))) mkString "\n"
-
-        def getTexts( p :Option[Any]): Seq[String] =  {
-            val results = p match {
-                case None => return Nil
-                case Some( x :Map[String,Any]) => x 
-            }
-            results("results") match {
-                case r: List[List[Tuple2[String,String]]] => r map (_(0)._2) 
-                case _ => Nil
-            }
+        results("results") match {
+            case r: List[List[Tuple2[String,String]]] => r map (_(0)._2) 
+            case _ => Nil
         }
+    }
 
-        def formatListing() = interests mkString ", "  
+    def formatListing() = interests mkString ", "  
 
-        def interests() = file(DEFAULT).listFiles map(_.getName)
+    def interests() = file(DEFAULT).listFiles map(_.getName)
 
-            def usage() = { 
-            println("usage:\n [follow <word>|show <word>|list]")
-            exit(1)
+    def usage() = { 
+        println("usage:\n [follow <word>|show <word>|list]")
+        exit(1)
+    }
+
+    def main(args: Array[String]) {
+        args match {
+            case Array("follow", word) => follow(word)
+            case Array("show", word)   => show(word)
+            case Array("list")         => list()
+            case _ => usage()
         }
-
-        def main(args: Array[String]) {
-            args match {
-                case Array("follow", word) => follow(word)
-                case Array("show", word)   => show(word)
-                case Array("list")         => list()
-                case _ => usage()
-            }
-        }
-        //}
+    }
+//}
 
 
-        main(args)
+main(args)
 
-        // vim: set ts=2 sw=2 et:
+// vim: set ts=2 sw=2 et:
