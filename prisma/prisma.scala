f36d1d6f63ee0dc53876cdbace7feabb3f179589
gabriele renzi <rff.rff@gmail.com>
2009-07-12 15:09:42 +0200

    parsing works

--- prisma/prisma.scala
+++ prisma/prisma.scala
@@ -1,15 +1,61 @@
-def follow(x: String)= println("follow "+x)
-def show (x: String)= println("show "+x)
-def list() = println("list")
+import java.io.File
+import scala.util.parsing.json.JSON.parseFull
 
-def usage() = { 
-    println("usage:\n [follow <word>|show <word>|list]")
-    exit(1)
-}
-args match {
-    case Array("follow", word) => follow(word)
-    case Array("show", word)   => show(word)
-    case Array("list")         => list()
-    case _ => usage()
-}
-// vim: set ts=4 sw=4 et:
+//object Prisma {
+    def mkPath(p : Seq[String]): String = p mkString File.separator
+    def file(segments: String*): File = {
+        val dir = new File(mkPath(segments.elements.toList.init))
+            dir.mkdirs
+        new File(dir,segments.last)
+    }
+    val DEFAULT = mkPath(List(System.getProperty("user.home"), ".prisma"))
+
+            def follow(x: String) = {
+            if (file(DEFAULT, x) createNewFile)
+                println("following "+x)
+            else
+                println("already following "+ x)
+        }
+
+        def show (x: String)= println("Data is "+ search(x))
+
+        def list() = println("Following\n"+ formatListing)
+
+        def read(key:String):String = io.Source.fromURL("http://search.twitter.com/search.json?q="+key) mkString
+
+        def search(key: String): String = getTexts(parseFull(read(key))) mkString "\n"
+
+        def getTexts( p :Option[Any]): Seq[String] =  {
+            val results = p match {
+                case None => return Nil
+                case Some( x :Map[String,Any]) => x 
+            }
+            results("results") match {
+                case r: List[List[Tuple2[String,String]]] => r map (_(0)._2) 
+                case _ => Nil
+            }
+        }
+
+        def formatListing() = interests mkString ", "  
+
+        def interests() = file(DEFAULT).listFiles map(_.getName)
+
+            def usage() = { 
+            println("usage:\n [follow <word>|show <word>|list]")
+            exit(1)
+        }
+
+        def main(args: Array[String]) {
+            args match {
+                case Array("follow", word) => follow(word)
+                case Array("show", word)   => show(word)
+                case Array("list")         => list()
+                case _ => usage()
+            }
+        }
+        //}
+
+
+        main(args)
+
+        // vim: set ts=2 sw=2 et:
